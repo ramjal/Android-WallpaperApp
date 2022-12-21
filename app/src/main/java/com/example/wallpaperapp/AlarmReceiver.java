12692967226c11fbb17b33662429b555446f3088
Ramin Jalali <ramjal@gmail.com>
2022-12-21 09:57:32 -0800

    Clean up

--- app/src/main/java/com/example/wallpaperapp/AlarmReceiver.java
+++ app/src/main/java/com/example/wallpaperapp/AlarmReceiver.java
@@ -1,31 +1,22 @@
 package com.example.wallpaperapp;
-
-import android.app.WallpaperManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.os.Build;
 import android.util.Log;
-
 import androidx.annotation.RequiresApi;
-
-import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
-import static android.app.WallpaperManager.FLAG_LOCK;
 import static android.content.Context.MODE_PRIVATE;
 
 public class AlarmReceiver extends BroadcastReceiver {
     private static final String LOG_TAG = AlarmReceiver.class.getSimpleName();
     private String[] imagesPathList;
 
-    private static final String SHARED_PREF_FILE_NAME = "com.example.wallpaperapp";
+    private static final String SHARED_PREF_FILE_NAME = "com.example.wallpaper";
     private int pictureIndex;
     private SharedPreferences mPreferences;
 
@@ -66,8 +57,8 @@ public class AlarmReceiver extends BroadcastReceiver {
     }
 
     private void LogSuccessMessage() {
-        String time = new SimpleDateFormat("HH:mm").format(new Date());
-        String message = String.format("Wallpaper set with image index=%d at %s", pictureIndex, time);
+        String time = new SimpleDateFormat("HH:mm", Locale.CANADA).format(new Date());
+        String message = String.format(Locale.CANADA, "Wallpaper set with image index=%d at %s", pictureIndex, time);
         Log.i(LOG_TAG, message);
     }
 
@@ -75,7 +66,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         pictureIndex++;
         SharedPreferences.Editor preferencesEditor = mPreferences.edit();
         preferencesEditor.putInt(MainActivity.IMAGE_INDEX, pictureIndex);
-        String time = new SimpleDateFormat("HH:mm").format(new Date());
+        String time = new SimpleDateFormat("HH:mm", Locale.CANADA).format(new Date());
         preferencesEditor.putString(MainActivity.LAST_ALARM, time);
         preferencesEditor.apply();
     }
